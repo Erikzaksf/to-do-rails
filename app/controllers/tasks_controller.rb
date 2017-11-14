e2c5546578116753d97db29b450b1d18e71862ea
erikzaksf <erik.zak.sf@gmail.com>
2017-11-14 10:52:16 -0800

    delete not working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,8 +5,6 @@ class TasksController < ApplicationController
     @task = @list.tasks.new
   end
 
-
-
   def create
     @list = List.find(params[:list_id])
     @task = @list.tasks.new(task_params)
@@ -15,6 +13,29 @@ class TasksController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @list = List.find(params[:list_id])
+    @task = Task.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @list = List.find(params[:list_id])
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to list_path(@task.list)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:list_id])
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to list_path(@list)
   end
 
   private
